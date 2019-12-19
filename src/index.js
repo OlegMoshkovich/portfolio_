eecada488ec96256b00e5d4ef25b8609ed6c28a1
Oleg Moshkovich <omoshkovich@gmail.com>
2019-12-19 16:12:18 -0500

    stripping off everything

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Portfolio } from './containers/portfolio';
-import { Context } from './containers/context'
 import * as serviceWorker from './serviceWorker';
-import { Router } from "@reach/router"
+
 
 
 
 const Application = () => {
     return (
-        <Router>
-            <Portfolio path="/" />
-            <Context path="context" />
-        </Router>
+        <div>hello</div>
+        // <Router>
+        //     <Portfolio path="/" />
+        //     <Context path="context" />
+        // </Router>
     )
 }
 
