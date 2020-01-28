ccf9f69e0a7e840f0eb695d578de9e14ec499003
Oleg Moshkovich <omoshkovich@gmail.com>
2020-01-28 17:07:22 -0500

    added more aec projects

--- src/containers/PortfolioBuilding.js
+++ src/containers/PortfolioBuilding.js
@@ -21,6 +21,14 @@ import gprom from '../img/building/resume4.png'
 import w from '../img/building/resume5.png'
 
 import overall from '../img/building/resume6.png'
+import resume7 from '../img/building/resume7.png'
+import resume8 from '../img/building/resume8.png'
+import resume9 from '../img/building/resume9.png'
+import resume10 from '../img/building/resume10.png'
+import resume11 from '../img/building/resume11.png'
+import resume12 from '../img/building/resume12.png'
+
+
 
 import { pageViewEvent } from '../actions/segment'
 import { connect } from 'react-redux'
@@ -55,20 +63,44 @@ const PortfolioBuilding = () => {
       <Element name="3" />
       <Hero color={'black'} fontColor={'white'} header={true} img={ribbon} proportions={'95%'} />
 
-      {/* -----------------ribbon */}
-      <Element name="10design" />
+
       <Element name="4" />
       <Hero color={'black'} fontColor={'white'} header={true} img={gprom} proportions={'95%'} />
 
-      {/* -----------------ribbon */}
-      <Element name="10design" />
+
       <Element name="5" />
       <Hero color={'black'} fontColor={'white'} header={true} img={w} proportions={'95%'} />
 
+
+      {/* <Element name="6" />
+      <Hero color={'black'} fontColor={'white'} header={true} img={overall} proportions={'95%'} /> */}
+
+
+      <Element name="7" />
+      <Hero color={'black'} fontColor={'white'} header={true} img={resume7} proportions={'95%'} />
+
+
+      <Element name="8" />
+      <Hero color={'black'} fontColor={'white'} header={true} img={resume8} proportions={'95%'} />
+
+
+      <Element name="9" />
+      <Hero color={'black'} fontColor={'white'} header={true} img={resume9} proportions={'95%'} />
+
+
+
+
+      <Element name="10" />
+      <Hero color={'black'} fontColor={'white'} header={true} img={resume10} proportions={'95%'} />
+
       {/* -----------------ribbon */}
       <Element name="10design" />
-      <Element name="6" />
-      <Hero color={'black'} fontColor={'white'} header={true} img={overall} proportions={'95%'} />
+      <Element name="11" />
+      <Hero color={'black'} fontColor={'white'} header={true} img={resume11} proportions={'95%'} />
+      <Element name="12" />
+      <Hero color={'black'} fontColor={'white'} header={true} img={resume12} proportions={'95%'} />
+
+
 
 
     </ThemeContext.Provider>
